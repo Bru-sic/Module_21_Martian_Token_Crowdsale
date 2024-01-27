c3dff76df3756446b6736c376e2bac3dd9c815b9
Bru-sic <brusic@exemail.com.au>
2024-01-27 16:04:46 +1100

    KaseiCoinCrowdsaleDeployer constructor

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Module_21_Martian_Token_Crowdsale
 Ethereum blockchain and virtual machine smart contracts built using the ERC-20 standard and OpenZepplin libraries to mint a fungible token ('KaseiCoin') and facilitate its crowdsale.
+
+
+
+# Deviations from instructions
+## Instructions at Step 2.4
+Requirement: Compile the contract by using compiler version 0.5.0.   
+Deviation: Compiled the contract using compiler version 0.5.5 and setting EVM to `constantinople`.   
+Justification: Using version 0.5.0 and the default EVM generated the following compile errors:
+> https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/utils/Address.sol:1:1: SyntaxError: Source file requires different compiler version (current compiler is 0.5.0+commit.1d4f565a.Emscripten.clang - note that nightly builds are considered to be strictly less than the released version
+pragma solidity ^0.5.5;   
+^---------------------^   
+   
+> .deps/github/OpenZeppelin/openzeppelin-contracts/contracts/utils/Address.sol:31:32: TypeError: The "extcodehash" instruction is only available for Constantinople-compatible VMs  (you are currently compiling for "byzantium").
+        assembly { codehash := extcodehash(account) }
+                               ^------------------^
--- Submission/Code/KaseiCoinCrowdsale.sol
+++ Submission/Code/KaseiCoinCrowdsale.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.5;
 
 /// @author Bruno Ivasic
 /// @title KaseiCoinCrowdsale: A smart contract for deploying and managing the KaseiCoin token crowdsale, and allowing investors to purchase KaseiCoin tokens with ether (ETH).
+
+/// @dev Note: requires compiler 0.5.5+ and EVM constantinople to avoid compiler errors and warnings.
 
 /// @dev Import the KaseiCoin contract from the local repository so it can be used as part of the crowdsale contract
 import "./KaseiCoin.sol";
@@ -12,17 +14,17 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
 
 
-/// @dev Create the KaseiCoinCrowdsale contract composed of the KaseiCoin token classs and inheriting the OpenZeppelin Crowdsale and MintedCrowdsale contract classes.
+/// @dev Define the KaseiCoinCrowdsale contract composed of the KaseiCoin token classs and inheriting the OpenZeppelin Crowdsale and MintedCrowdsale contract classes.
 contract KaseiCoinCrowdsale is Crowdsale, MintedCrowdsale {
-    /** @notice
-     * Defines the KaseiCoinCrowdsale constructor which is called once only when the smart contract is deployed.
+    /** @notice Defines the KaseiCoinCrowdsale constructor which is called once only when the smart contract is deployed.
      * The KaseiCoinCrowdsale constructor calls the Crowdsale constructor.
      * @param rate The number of KaseiCoin token units a buyer receives for each wei.
      * @dev `rate` is the conversion between wei and the smallest and indivisible
      * KaseiCoin token unit. As, the KaseiCoin token has been created with a `decimal` of '18' then
-     * a `rate` of 1 means the investor would receive 1E-18 (0.000,000,000,000,000,001) of KaseiCoins per wei.
-     * @param wallet Address where collected funds will be forwarded to - ie usually the Crowdsale Initiator's wallet.
-     * @param token Address of the KaseiCoin token smart contract being sold once deployed.
+     * a `rate` of 1 means the investor would receive 1E-18 (0.000,000,000,000,000,001) KaseiCoin tokens per wei or in
+     * other words, 1 Token for each ETH.
+     * @param wallet Address where collected funds will be forwarded to - ie usually the Crowdsale organiser's wallet.
+     * @param token The KaseiCoin token smart contract being sold once deployed.
      */
     constructor(
         uint rate,
@@ -33,36 +35,51 @@ contract KaseiCoinCrowdsale is Crowdsale, MintedCrowdsale {
     }
 }
 
-
-
-
-/*
+/** @notice Define the KaseiCoinCrowdsaleDeployer contract, which automates deployment of the KaseiCoinCrowdsale and the KaseiCoin contracts.
+ *  * Creates an instance of the KaseiCoin token contract, storing the KaseiCoin contract's deployment address;
+ *  * Creates a KaseiCoinCrowdsale contract to facilitate investors purchasing the token, storing the KaseiCoinCrowdsale contract's deployment address;
+ *  * Assigns the minter role of KaseiCoin tokens to the KaseiCoinCrowdsale contract; and
+ *  * Renounces the minter role from the original creator of the KaseiCoin contract to avoid any potential for nefarious minting. 
+ */
 contract KaseiCoinCrowdsaleDeployer {
-    // Create an `address public` variable called `kasei_token_address`.
-    // YOUR CODE HERE!
-    // Create an `address public` variable called `kasei_crowdsale_address`.
-    // YOUR CODE HERE!
+    /// @dev Public address of the deployed KaseiCoin smart contract.
+    address public kasei_token_address;    
+    /// @dev Public address of the deployed KaseiCoinCrowdsale smart contract.
+    address public kasei_crowdsale_address;
 
-    // Add the constructor.
+    /** @notice Define the KaseiCoinCrowdsaleDeployer constructor which is called once only when the KaseiCoinCrowdsaleDeployer smart contract is deployed.
+     * The KaseiCoinCrowdsaleDeployer constructor:
+     *  * Creates an instance of the KaseiCoin token contract, storing the KaseiCoin contract's deployment address in `kasei_token_address`;
+     *  * Creates a KaseiCoinCrowdsale contract to facilitate investors purchasing the token, storing the KaseiCoinCrowdsale contract's deployment address in `kasei_crowdsale_address`;
+     *  * Assigns the minter role of KaseiCoin tokens to the KaseiCoinCrowdsale contract; and
+     *  * Renounces the minter role from the original `msg.sender` to avoid any potential for nefarious minting. 
+     *  
+     * @param name The name given to the KaseiCoin token.
+     * @param symbol The (ticker) symbol given to the KaseiCoin token.
+     * @param wallet The Ethereum Address that will receive the funding as investors purchase KaseiCoin tokens via the KaseiCoinCrowdsale contract.
+     */
     constructor(
-       // YOUR CODE HERE!
-    ) public {
-        // Create a new instance of the KaseiCoin contract.
-        // YOUR CODE HERE!
-        
-        // Assign the token contract’s address to the `kasei_token_address` variable.
-        // YOUR CODE HERE!
+        string memory name,
+        string memory symbol,
+        address payable wallet ) public {
 
-        // Create a new instance of the `KaseiCoinCrowdsale` contract
-        // YOUR CODE HERE!
-            
-        // Aassign the `KaseiCoinCrowdsale` contract’s address to the `kasei_crowdsale_address` variable.
-        // YOUR CODE HERE!
-        // Set the `KaseiCoinCrowdsale` contract as a minter
-        // YOUR CODE HERE!
+        /// @dev Create an instance of the `KaseiCoin` token contract,
+        /// @dev with an initial supply of 0 tokens based on the `name` and `symbol` provided at time of creation by the Crowdsale organiser.
+        KaseiCoin kasei_token = new KaseiCoin(name, symbol, 0); 
         
-        // Have the `KaseiCoinCrowdsaleDeployer` renounce its minter role.
-        // YOUR CODE HERE!
+        /// @dev Store a local copy of the KaseiCoin's token address so that it is all linked and accessible for ease of traceability and auditing.
+        kasei_token_address = address(kasei_token);
+
+        /// @dev Create an instance of the `KaseiCoinCrowdsale` contract with an exchange rate of 1 Ether to 1 Token, the wallet address of theCrowdsale organiser and the KaseiCoin contract.
+        KaseiCoinCrowdsale kasei_crowdsale = new KaseiCoinCrowdsale(1, wallet, kasei_token);
+          
+        /// @dev Store a local copy of the KaseiCoinCrowdsale's address so that it is all linked and accessible for ease of traceability and auditing.
+        kasei_crowdsale_address = address(kasei_crowdsale);
+
+        /// @dev Assign the `KaseiCoinCrowdsale` contract as a minter of the KaseiCoin token contract
+        kasei_token.addMinter(kasei_crowdsale_address);
+        
+        /// @dev Renounce the `KaseiCoinCrowdsaleDeployer` contract as a minter of the KaseiCoin token contract to avoid any potential for nefarious minting.
+        kasei_token.renounceMinter();
     }
 }
-*/
